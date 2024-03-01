47e176024147a6718f5a84c36711efcc284fe6bd
James Best <jim@justjibba.net>
2024-03-01 13:24:25 +0000

    Add retriever

--- app/chat/vector_stores/pinecone.py
+++ app/chat/vector_stores/pinecone.py
@@ -2,6 +2,7 @@ import os
 import pinecone
 from langchain.vectorstores import Pinecone
 from app.chat.embeddings.openai import embeddings
+from app.chat.models import ChatArgs
 
 pinecone.init(
     api_key=os.getenv("PINECONE_API_KEY"), environment=os.getenv("PINECONE_ENV_NAME")
@@ -10,3 +11,8 @@ pinecone.init(
 vector_store = Pinecone.from_existing_index(
     os.getenv("PINECONE_INDEX_NAME"), embeddings
 )
+
+
+def build_retriever(chat_args: ChatArgs):
+    search_kwargs = {"filter": {"pdf_id": chat_args.pdf_id}}
+    return vector_store.as_retriever(search_kwargs=search_kwargs)
