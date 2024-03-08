4704938e3b4b65219bfe00828b62562be8324782
James Best <jim@justjibba.net>
2024-03-08 05:33:36 +0000

    make select component functio

--- app/chat/chat.py
+++ app/chat/chat.py
@@ -3,9 +3,24 @@ from app.chat.chains.retrieval import StreamingConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 from app.chat.models import ChatArgs
 from app.chat.vector_stores import retriever_map
-from app.chat.llms.chatopenai import build_llm
-from app.chat.memories.sql_memory import build_memory
+from app.chat.llms import llm_map
+from app.chat.memories import memory_map
 from app.web.api import set_conversation_components, get_conversation_components
+
+
+def select_component(component_type, component_map, chat_args):
+    components = get_conversation_components(chat_args.conversation_id)
+
+    previous_component = components[component_type]
+
+    if previous_component:
+        builder = component_map[previous_component]
+        return previous_component, builder(chat_args)
+
+    else:
+        random_name = random.choice(list(component_map.keys()))
+        builder = component_map[random_name]
+        return random_name, builder(chat_args)
 
 
 def build_chat(chat_args: ChatArgs):
@@ -19,33 +34,40 @@ def build_chat(chat_args: ChatArgs):
 
         chain = build_chat(chat_args)
     """
-    components = get_conversation_components(chat_args.conversation_id)
-    previous_retriever = components["retriever"]
-    retriever = None
-    if previous_retriever:
-        # this is NOT the first message of the conversation and
-        # I need to use the same retriever
-        build_retriever = retriever_map[previous_retriever]
-        retriever = build_retriever(chat_args)
-    else:
-        # this is the first and I need to pick a random retriever
-        random_retriever_name = random.choice(list(retriever_map))
-        build_retriever = retriever_map[random_retriever_name]
-        retriever = build_retriever(chat_args)
-        set_conversation_components(
-            conversation_id=chat_args.conversation_id,
-            llm="",
-            memory="",
-            retriever=random_retriever_name,
-        )
+    retriver_name, retriever = select_component(
+        "retriever",
+        retriever_map,
+        chat_args,
+    )
 
-    llm = build_llm(chat_args)
+    llm_name, llm = select_component(
+        "llm",
+        llm_map,
+        chat_args,
+    )
+
+    memory_name, memory = select_component(
+        "memory",
+        memory_map,
+        chat_args,
+    )
+
+    set_conversation_components(
+        chat_args.conversation_id,
+        llm=llm_name,
+        retriever=retriver_name,
+        memory=memory_name,
+    )
+
+    print(
+        f"Running chain with: \n\n memory: {memory_name}, llm: {llm_name}, retriver: {retriver_name}"
+    )
+
     # A second llm is created for use with the condense uestion chain to help
     # with the fact that callbacks are shared across all objects in a chain
     # By using a separate llm we can set streaming to false and then filter based on
     # that property in the callback
     condense_question_llm = ChatOpenAI(streaming=False)
-    memory = build_memory(chat_args)
 
     return StreamingConversationalRetrievalChain.from_llm(
         llm=llm,
