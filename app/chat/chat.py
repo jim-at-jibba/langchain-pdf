bb8cbf7bb6f5284ecb25a91bf7461a938210b5ac
James Best <jim@justjibba.net>
2024-03-08 05:14:20 +0000

    Create random component picker for the retriever

--- app/chat/chat.py
+++ app/chat/chat.py
@@ -1,9 +1,11 @@
+import random
 from app.chat.chains.retrieval import StreamingConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 from app.chat.models import ChatArgs
-from app.chat.vector_stores.pinecone import build_retriever
+from app.chat.vector_stores import retriever_map
 from app.chat.llms.chatopenai import build_llm
 from app.chat.memories.sql_memory import build_memory
+from app.web.api import set_conversation_components, get_conversation_components
 
 
 def build_chat(chat_args: ChatArgs):
@@ -17,7 +19,26 @@ def build_chat(chat_args: ChatArgs):
 
         chain = build_chat(chat_args)
     """
-    retriever = build_retriever(chat_args)
+    components = get_conversation_components(chat_args.conversation_id)
+    previous_retriever = components["retriever"]
+    retriever = None
+    if previous_retriever:
+        # this is NOT the first message of the conversation and
+        # I need to use the same retriever
+        build_retriever = retriever_map[previous_retriever]
+        retriever = build_retriever(chat_args)
+    else:
+        # this is the first and I need to pick a random retriever
+        random_retriever_name = random.choice(list(retriever_map))
+        build_retriever = retriever_map[random_retriever_name]
+        retriever = build_retriever(chat_args)
+        set_conversation_components(
+            conversation_id=chat_args.conversation_id,
+            llm="",
+            memory="",
+            retriever=random_retriever_name,
+        )
+
     llm = build_llm(chat_args)
     # A second llm is created for use with the condense uestion chain to help
     # with the fact that callbacks are shared across all objects in a chain
